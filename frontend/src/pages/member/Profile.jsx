e51a8161ab7d035379c07eeb72029ff2b7695435
shaolin-peanut <shaolinpeanut@gmail.com>
2024-10-04 15:51:52 +0200

    factored out buttons, now in interaction.js, will make redesign easier

--- frontend/src/pages/member/Profile.jsx
+++ frontend/src/pages/member/Profile.jsx
@@ -7,19 +7,17 @@ import PicGallery from '@/components/PicGallery';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import { ThumbsUp, UserX, AlertTriangle } from "lucide-react";
 import CustomLayout from '@/components/MatchaLayout';
-import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { toggleLike, toggleBlock, markView } from "@/api"
+import { markView } from "@/api"
 import ProfileForm from '@/components/CustomProfileForm';
 import { useRedirectIfLoggedOut } from '@/hooks/useRedirectLogout'
+import { Interaction } from '@/components/Interaction';
 
 const ProfilePage = () => {
   const { user } = useUser();
   useRedirectIfLoggedOut();
   
   const location = useLocation();
-  const queryClient = useQueryClient();
   
   const [isEditMode, setIsEditMode] = useState(false);
   
@@ -30,67 +28,11 @@ const ProfilePage = () => {
   
   const { data: userInfo, isLoading, error } = useUserData(profileUsername, user?.username);
 
-  const likeMutation = useMutation({
-    mutationFn: toggleLike,
-    onMutate: async ({ profileUsername, viewer, isLiked }) => {
-      await queryClient.cancelQueries(['userData', profileUsername, viewer]);
-      const previousUserInfo = queryClient.getQueryData(['userData', profileUsername, viewer]);
-      queryClient.setQueryData(['userData', profileUsername, viewer], old => ({
-        ...old,
-        isLiked: !isLiked
-      }));
-      return { previousUserInfo };
-    },
-    onError: (err, variables, context) => {
-      queryClient.setQueryData(['userData', variables.profileUsername, variables.viewer], context.previousUserInfo);
-    },
-    onSettled: (data, error, { profileUsername, viewer }) => {
-      queryClient.invalidateQueries(['userData', profileUsername, viewer]);
-    }
-  });
-
-  const blockMutation = useMutation({
-    mutationFn: toggleBlock,
-    onMutate: async ({ profileUsername, viewer, isBlocked }) => {
-      await queryClient.cancelQueries(['userData', profileUsername, viewer]);
-      const previousUserInfo = queryClient.getQueryData(['userData', profileUsername, viewer]);
-      queryClient.setQueryData(['userData', profileUsername, viewer], old => ({
-        ...old,
-        isBlocked: !isBlocked,
-        isLiked: !isBlocked ? false : old.isLiked // Unlike if blocking
-      }));
-      return { previousUserInfo };
-    },
-    onError: (err, variables, context) => {
-      queryClient.setQueryData(['userData', variables.profileUsername, variables.viewer], context.previousUserInfo);
-    },
-    onSettled: (data, error, { profileUsername, viewer }) => {
-      queryClient.invalidateQueries(['userData', profileUsername, viewer]);
-    }
-  });
-
-  const handleLike = () => {
-    if (user && profileUsername && (user?.username != profileUsername)) {
-      likeMutation.mutate({ profileUsername, viewer: user?.username, isLiked: userInfo.isLiked });
-    }
-  };
-  
-  const handleBlock = () => {
-    if (user && profileUsername && (user?.username != profileUsername)) {
-      blockMutation.mutate({ profileUsername, viewer: user.username, isBlocked: userInfo.isBlocked });  
-    }
-  };
-
-  const handleReport = () => {
-    alert("This user has been reported!!");
-  }
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   const { displayUser, isLiked, isBlocked } = userInfo ?? {};
   const isSelf = user?.username === profileUsername;
-  const hasPics = displayUser?.pics?.length > 0;
   if (!displayUser?.viewed_by?.includes(user?.username) && !isSelf) {
     markView({ profileUsername: profileUsername, viewer: user?.username });
   }
@@ -140,31 +82,16 @@ const ProfilePage = () => {
             <p><strong>Viewed by:</strong> {displayUser?.viewed_by ?? 'N/A'}</p>
           </div>
 
-          {!isSelf ? (
+          {isSelf ? (
             <div className="flex justify-between mt-4">
-              <Button onClick={handleLike}
-                variant={isLiked ? "default" : "outline"}
-                disabled={isBlocked || !hasPics}
-              >
-                <ThumbsUp className="mr-2 h-4 w-4" /> {isLiked ? 'Unlike' : 'Like'}
+            { !isEditMode &&
+              <Button onClick={() => setIsEditMode(true)} variant="outline">
+                Edit Profile
               </Button>
-              <Button onClick={handleBlock}
-                variant={isBlocked ? "default" : "outline"}
-              >
-                <UserX className="mr-2 h-4 w-4" /> {isBlocked ? 'Unblock' : 'Block'}
-              </Button>
-              <Button onClick={handleReport} variant="outline">
-                <AlertTriangle className="mr-2 h-4 w-4" /> Report
-              </Button>
-            </div>
+            }
+          </div>
           ) : (
-            <div className="flex justify-between mt-4">
-              { !isEditMode &&
-                <Button onClick={() => setIsEditMode(true)} variant="outline">
-                  Edit Profile
-                </Button>
-              }
-            </div>
+            <Interaction profileUsername={profileUsername} user={user}/>
           )}
         </CardContent>
       </Card>
