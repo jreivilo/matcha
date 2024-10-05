8263c85e6f40eebe1370380ce26774b4c2b914ce
shaolin-peanut <shaolinpeanut@gmail.com>
2024-10-05 23:59:13 +0200

    simplified geolocation function

--- frontend/src/hooks/useGeoLocation.js
+++ frontend/src/hooks/useGeoLocation.js
@@ -1,29 +1,22 @@
 import { useState, useEffect } from 'react';
 
-export const useGeoLocation = (isInitialSetup) => {
-  const [coordinates, setCoordinates] = useState('');
-  const [isLoadingCoordinates, setIsLoadingCoordinates] = useState(false);
-  
-  useEffect(() => {
-    setIsLoadingCoordinates(true);
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setCoordinates(`${position.coords.latitude}, ${position.coords.longitude}`);
-          setIsLoadingCoordinates(false);
-        },
-        async (error) => {
-          console.log('User denied geolocation access, but we will find it anyway:', error);
-          try {
-            const response = await fetch("http://ip-api.com/json");
-            const geoData = await response.json();
-            setCoordinates(`${geoData.lat}, ${geoData.lon}`);
-          } catch (ipError) {
-            console.error('Error fetching geo info:', ipError);
-          }
-          setIsLoadingCoordinates(false);
-        }
-      );
-  }, [isInitialSetup == true]);
-
-  return { coordinates, isLoadingCoordinates, setCoordinates };
+export const useGeoLocation = () => { 
+  let coordinates = '';
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      coordinates = `${position.coords.latitude}, ${position.coords.longitude}`;
+    },
+    async (error) => {
+      console.log('User denied geolocation access, but we will find it anyway:', error);
+      try {
+        const response = await fetch("http://ip-api.com/json");
+        const geoData = await response.json();
+        coordinates = `${geoData.lat}, ${geoData.lon}`;
+        console.log(coordinates);
+      } catch (ipError) {
+        console.error('Error fetching geo info:', ipError);
+      }
+    }
+  );
+  return coordinates;
 };
